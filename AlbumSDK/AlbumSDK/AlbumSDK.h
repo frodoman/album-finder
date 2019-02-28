f01e9c037c0f99cbc8dc670f94e1ca53fe24635c
xing-liu <xinghou.liu@sapient.com>
2019-02-28 08:55:01 +0000

    Create work space and project files.

---
+++ AlbumSDK/AlbumSDK/AlbumSDK.h
@@ -0,0 +1,19 @@
+//
+//  AlbumSDK.h
+//  AlbumSDK
+//
+//  Created by Xinghou Liu on 28/02/2019.
+//  Copyright © 2019 Xing Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlbumSDK.
+FOUNDATION_EXPORT double AlbumSDKVersionNumber;
+
+//! Project version string for AlbumSDK.
+FOUNDATION_EXPORT const unsigned char AlbumSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlbumSDK/PublicHeader.h>
+
+
