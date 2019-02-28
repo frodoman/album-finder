cee053ffa7630851b68b02e96f4cfa389efa2511
xing-liu <xinghou.liu@sapient.com>
2019-02-28 16:13:20 +0000

    Add network service file.

---
+++ AlbumSDK/AlbumSDK/Networking/AlbumService.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumService.swift
+//  AlbumSDK
+//
+//  Created by Xinghou Liu on 28/02/2019.
+//  Copyright © 2019 Xing Liu. All rights reserved.
+//
+
+import Foundation
+
+public enum ApiAccount {
+    public static let ApiKey = "ee24253219aeeeadb26ed0033ca1b1e2"
+    public static let sharedSecret = "ad67b042f9d44f9f5a2dce1758be8105"
+    public static let appName = "AlbumFinder"
+}
+
